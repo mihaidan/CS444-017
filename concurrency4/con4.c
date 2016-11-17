84440fd3176fc63c65e67d6124271d3eaacc281e
mihaidan <danm@oregonstate.edu>
2016-11-17 09:34:22 -0800

    con4 set-up

---
+++ concurrency4/con4.c
@@ -0,0 +1,16 @@
+/***********************************************
+* Concurrency 4 - The Barbershop Problem *
+* Christian Armatas & Mihai Dan     		   *
+* 11/17/2016             					   *
+***********************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include "mt19937ar.h"
+
